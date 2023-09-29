8a44ff41eeff15c7479c22e3be4482291ba521ae
Epikle <98042337+Epikle@users.noreply.github.com>
2023-09-29 17:03:07 +0300

    :recycle: Clean

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -6,8 +6,6 @@ import { db } from "@/lib/db";
 export const updateProfile = async (userId: string, values: FormValues) => {
   const newLinks = values.links.map((link) => ({ ...link, userId }));
 
-  console.log(values);
-
   await db.link.deleteMany({ where: { userId } });
   await db.link.createMany({ data: newLinks });
   await db.user.update({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import LinkForm from "@/components/LinkForm";
-import TestLogout from "@/components/TestLogout";
-import * as C from "@/components/ui/Card";
-import { getAuthSession } from "@/lib/auth";
-import { db } from "@/lib/db";
-import { Link as LinkIcon } from "lucide-react";
 import { Metadata } from "next";
 import Link from "next/link";
+import { Link as LinkIcon } from "lucide-react";
+
+import LinkForm from "@/components/LinkForm";
+import TestLogout from "@/components/TestLogout";
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
 
 export const metadata: Metadata = {
   title: "Homepage",
@@ -37,34 +37,32 @@ export default async function Home() {
 
   return (
     <>
-      <header>
-        <C.Card className="flex items-center justify-between px-4 py-2">
-          <div className="flex items-center gap-8">
-            <Link
-              href="/"
-              className="flex items-center gap-2 rounded-sm px-4 py-2 transition-colors hover:bg-gray-400"
-            >
-              <LinkIcon />
-              <span>
-                oktaani<span className="font-extrabold">LINK</span>
-              </span>
+      <header className="flex items-center justify-between border-b-2 bg-gray-100 px-4 py-2">
+        <div className="flex items-center gap-8">
+          <Link
+            href="/"
+            className="flex items-center gap-2 rounded-sm px-4 py-2 transition-colors hover:bg-gray-400"
+          >
+            <LinkIcon />
+            <span>
+              oktaani<span className="font-extrabold">LINK</span>
+            </span>
+          </Link>
+          <ul>
+            <Link href="/view" className="hover:underline">
+              <li>Explore Profiles</li>
             </Link>
-            <ul>
-              <Link href="/view" className="hover:underline">
-                <li>Explore Profiles</li>
-              </Link>
-            </ul>
-          </div>
-          <nav>
-            {session && (
-              <TestLogout
-                name={session.user.name}
-                imgUrl={session.user.image}
-                email={session.user.email}
-              />
-            )}
-          </nav>
-        </C.Card>
+          </ul>
+        </div>
+        <nav>
+          {session && (
+            <TestLogout
+              name={session.user.name}
+              imgUrl={session.user.image}
+              email={session.user.email}
+            />
+          )}
+        </nav>
       </header>
       <div className="container mt-4 text-primary">
         <LinkForm data={user} />
