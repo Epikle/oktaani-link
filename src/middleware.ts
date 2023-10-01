74c4b23f3d88850b302e34edb65347abf747ca15
Epikle <98042337+Epikle@users.noreply.github.com>
2023-10-01 16:16:47 +0300

    :bug: Fix auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,17 @@
-import { getToken } from 'next-auth/jwt';
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req });
 
   if (!token) {
-    return NextResponse.redirect(new URL('/login', req.url));
+    return NextResponse.redirect(new URL("/login", req.url));
   }
 }
 
 export const config = {
-  matcher: '/',
+  matcher: ["/", "/view"],
 };
 
 // export { default } from 'next-auth/middleware';
